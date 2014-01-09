41bca5b9fc1cbac48e3de4d2b7db5524b8f6acf2
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-01-09 14:44:35 -0700

    Added stubs for smart map

---
+++ team009/postProcessing/SmartMap.java
@@ -0,0 +1,33 @@
+package team009.postProcessing;
+
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+public class SmartMap extends BasePostProcessor {
+
+    public SmartMap(RobotController rc) {
+        super(rc);
+    }
+
+    @Override
+    public boolean calculate(int roundLimit) {
+        return false;
+    }
+
+    /**
+     * NOTE: you have to make sure canMove = false before calling this
+     * @param loc the location to check
+     * @return the index of the first obstacle that loc is part of
+     * //TODO: what do do if it's part of multiple?
+     */
+    public int getObstacleIndexForLocation(MapLocation loc) {
+        int index = -1;
+
+        return index;
+    }
+
+    public boolean canMove(MapLocation loc) {
+        return false;
+    }
+
+}
