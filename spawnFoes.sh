cad1df7819d51cd2a88df2a418ccfb3ba188f888
Primeagen <michael.b.paulson@gmail.com>
2014-01-08 17:06:29 -0800

    Working it

--- spawnFoes.sh
+++ spawnFoes.sh
@@ -3,15 +3,17 @@
 
 spawnFoe() {
     local branch=$1
+    local underscoredName=""
     local folderName="_team$1"
     git checkout $branch
 
-    echo "rm -rdf ./$folderName"
+    underscoredName=sed -e 's/[a-zA-Z]/X/g' -e 's/[0-9]/N/g'
+
     eval "rm -rdf ./$folderName"
     eval "mkdir ./$folderName"
-    eval "cp -r ./team009 ./$folderName/"
+    eval "cp -r ./team009 ./$folderName"
     eval "find ./$folderName -name '*.java' -type f -exec sed -i.bak 's/team009/$folderName/g' {} +"
-    eval "rm -f ./$folderName/*.bak"
+    eval "rm -rf ./$folderName/**/*.bak"
 }
 
 spawnFoe "0.1.1"
