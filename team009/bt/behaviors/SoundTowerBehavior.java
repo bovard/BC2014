41d451d7a69115ae087ceb65d23b9c30a41af055
Devin Gray <devin.gray@webfilings.com>
2014-01-12 22:28:16 -0700

    improve spiral

--- team009/bt/behaviors/SoundTowerBehavior.java
+++ team009/bt/behaviors/SoundTowerBehavior.java
@@ -39,17 +39,16 @@ public class SoundTowerBehavior extends Behavior {
         //spin around in a cirle shooting the gun
         int x = (int) (radius * java.lang.Math.cos(java.lang.Math.toRadians(angle))) + (robot.currentLoc.x);
         int y = (int) (radius * java.lang.Math.sin(java.lang.Math.toRadians(angle))) + (robot.currentLoc.y);
-        angle = angle+20;
+        angle = angle+50;
         if(angle >= 360) {
             angle = 0;
-            radius--;
-            if(radius<=0) {
+            radius = radius-1;
+            if(radius<=6) {
                 radius = MAX_DISTANCE; //range of the noise tower
             }
         }
         MapLocation loc = new MapLocation(x,y);
-        robot.rc.attackSquareLight(loc);
-        //robot.rc.attackSquare(loc);
+        robot.rc.attackSquare(loc);
         return true;
     }
 
