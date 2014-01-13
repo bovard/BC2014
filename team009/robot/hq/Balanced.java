c6629aefc29bd4b947da3e69568c8d60af9cd3cf
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-01-12 17:59:34 -0700

    Fixed balanced

--- team009/robot/hq/Balanced.java
+++ team009/robot/hq/Balanced.java
@@ -3,6 +3,7 @@ package team009.robot.hq;
 import battlecode.common.RobotController;
 import team009.RobotInformation;
 import team009.bt.Node;
+import team009.bt.decisions.hq.BalancedSelector;
 
 public class Balanced extends HQ {
     public Balanced(RobotController rc, RobotInformation info) {
@@ -12,6 +13,6 @@ public class Balanced extends HQ {
 
     @Override
     protected Node getTreeRoot() {
-        return null;
+        return new BalancedSelector(this);
     }
 }
