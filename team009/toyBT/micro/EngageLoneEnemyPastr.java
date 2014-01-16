62dbf24b86bb6c0a73407cee15313da189a1462d
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-01-15 21:18:32 -0700

    Started to fill in micro

--- team009/toyBT/micro/EngageLoneEnemyPastr.java
+++ team009/toyBT/micro/EngageLoneEnemyPastr.java
@@ -1,6 +1,8 @@
 package team009.toyBT.micro;
 
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
 import team009.bt.behaviors.Behavior;
 import team009.robot.TeamRobot;
 import team009.robot.soldier.ToySoldier;
@@ -17,6 +19,8 @@ public class EngageLoneEnemyPastr extends Behavior {
 
     @Override
     public boolean run() throws GameActionException {
+        RobotInfo pastr = ((ToySoldier)robot).enemyPastrs.arr[0];
+        MapLocation pastrLocation = pastr.location;
         return false;
     }
 }
