f0fbffd356c2ee045cc4e424a0bcf34897976ca0
Devin Gray <devin.gray@webfilings.com>
2014-01-21 21:13:12 -0700

    its broke

--- team009/toyBT/behaviors/ToyHerdReplace.java
+++ team009/toyBT/behaviors/ToyHerdReplace.java
@@ -10,6 +10,7 @@ import team009.robot.soldier.ToySoldier;
 public class ToyHerdReplace extends Behavior {
     protected BugMove move;
     protected MapLocation pastureLocation;
+    protected MapLocation soundLocation;
     protected ToySoldier soldier;
 
     public ToyHerdReplace(ToySoldier robot) {
@@ -23,7 +24,17 @@ public class ToyHerdReplace extends Behavior {
     @Override
     public boolean pre() throws GameActionException {
         // we can sense the square and we see that there isn't a pastr there!
-        return robot.rc.canSenseSquare(soldier.comLocation) && (robot.rc.senseObjectAtLocation(soldier.comLocation) == null || robot.currentLoc.equals(soldier.comLocation));
+        if (!pastureLocation.equals(soldier.comLocation)) {
+            pastureLocation = soldier.comLocation;
+            //set ideal location of sound tower
+            soundLocation = getTowerLocation();
+
+
+            move.setDestination(pastureLocation);
+        }
+        return robot.rc.canSenseSquare(soldier.comLocation)
+                && (robot.rc.senseObjectAtLocation(soldier.comLocation) == null
+                    || robot.currentLoc.equals(soldier.comLocation));
     }
 
     @Override
@@ -48,5 +59,46 @@ public class ToyHerdReplace extends Behavior {
             move.move();
         }
         return true;
+    }
+
+    public MapLocation getTowerLocation()
+    {
+        //first check the spots next to the hq that are also next to the pasture (optimal spots)
+        int hqx = hq.currentLoc.x;
+        int hqy = hq.currentLoc.y;
+        int px = pasture.x;
+        int py = pasture.y;
+        MapLocation[] optimalLocs = new MapLocation[4];
+
+        if (hqx != px && hqy != py) {
+            //only two optimal neighbors
+            optimalLocs[0] = new MapLocation(hqx, py);
+            optimalLocs[1] = new MapLocation(hqx, py);
+        }
+        else {
+            //4 optimal neighbors
+            if(hqx == px){
+                optimalLocs[0] = new MapLocation(hqx-1, hqy);
+                optimalLocs[1] = new MapLocation(hqx-1, py);
+                optimalLocs[2] = new MapLocation(hqx+1, hqy);
+                optimalLocs[3] = new MapLocation(hqx+1, py);
+            }
+            else {
+                optimalLocs[0] = new MapLocation(hqx, hqy-1);
+                optimalLocs[1] = new MapLocation(px, hqy+1);
+                optimalLocs[2] = new MapLocation(hqx, hqy-1);
+                optimalLocs[3] = new MapLocation(px, hqy-1);
+            }
+        }
+
+        for(int i=0; i<optimalLocs.length; i++) {
+            if(optimalLocs[i] != null && hq.rc.canMove(hq.currentLoc.directionTo(optimalLocs[i]))) {
+                return optimalLocs[i];
+            }
+        }
+
+        //otherwise get random move
+        MapLocation tower = hq.currentLoc.add(hq.getRandomSpawnDirection());
+        return tower;
     }
 }
