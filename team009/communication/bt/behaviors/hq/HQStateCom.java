d9e11f03dfa4be5a6f10c090a0d59d531decd0c7
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-01-29 20:04:05 -0700

    Hooked up the request to the HQ

--- team009/communication/bt/behaviors/hq/HQStateCom.java
+++ team009/communication/bt/behaviors/hq/HQStateCom.java
@@ -1,22 +1,41 @@
 package team009.communication.bt.behaviors.hq;
 
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import team009.communication.Communicator;
 import team009.communication.bt.behaviors.ReadBehavior;
 import team009.communication.decoders.TwoWayDecoder;
+import team009.hq.HQPreprocessor;
 import team009.robot.TeamRobot;
 import team009.hq.HQ;
 import team009.utils.SmartMapLocationArray;
 
+import java.util.ArrayList;
+
 public class HQStateCom extends ReadBehavior {
     HQ hq;
+
+    private MapLocation from;
+    private MapLocation to;
 
     public HQStateCom(HQ hq) {
         super(hq);
         this.hq = hq;
+
     }
 
     public boolean run() throws GameActionException {
+        // if we have an aStar and we are processing, process away!
+        if (((HQPreprocessor)hq).a != null && to != null && from != null) {
+            MapLocation done = ((HQPreprocessor)hq).a.getNextWayPoint(from, to);
+            // the Map Location will be non null if it's done processing!
+            if (done != null) {
+                from = null;
+                to = null;
+            }
+        }
+
+
         hq.soldierCounts = Communicator.ReadTypeAndGroup(rc, TeamRobot.SOLDIER_TYPE_TOY_SOLDIER, 0);
         hq.pastrCounts = Communicator.ReadTypeAndGroup(rc, TeamRobot.SOLDIER_TYPE_PASTR, TeamRobot.PASTR_GROUP);
         hq.noiseCounts = Communicator.ReadTypeAndGroup(rc, TeamRobot.SOLDIER_TYPE_NOISE_TOWER, TeamRobot.NOISE_TOWER_GROUP);
@@ -28,6 +47,33 @@ public class HQStateCom extends ReadBehavior {
                 hq.noiseLocations.add(dec.from);
             } else if (dec.command == TeamRobot.POSITION_OF_PASTR) {
                 hq.pastrLocations.add(dec.from);
+            } else if (dec.command == TeamRobot.REQUEST_LOCATION) {
+                if (((HQPreprocessor)hq).a != null) {
+                    MapLocation from = dec.from;
+                    MapLocation to = dec.to;
+                    MapLocation result = ((HQPreprocessor)hq).a.getCachedWayPoint(from, to);
+                    if (result == null) {
+                        // we haven't found a map location for you, sorry!
+                        // if we don't have a job processing, process this one!
+                        // this seems like the best lowest/cost idea
+                        if (from == null && to == null) {
+                            this.from = from;
+                            this.to = to;
+                        }
+                        // we don't have a position for you, better luck next time
+                        // TODO: Michael we didn't find a location, send failure back
+                        // Communicator.WriteTwoWayCommunicate(rc, TeamRobot.LOCATION_RESULT);
+                    } else {
+                        // we do have a way point for you, sending it back
+                        // TODO: Michael send result back
+                        // Communicator.WriteTwoWayCommunicate(rc, TeamRobot.LOCATION_RESULT);
+                    }
+
+
+
+
+
+                }
             }
             Communicator.ClearTwoWayChannel(rc, i);
         }
--- team009/hq/HQPreprocessor.java
+++ team009/hq/HQPreprocessor.java
@@ -2,7 +2,6 @@ package team009.hq;
 
 import battlecode.common.Clock;
 import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import team009.RobotInformation;
 import team009.utils.*;
@@ -14,9 +13,8 @@ public abstract class HQPreprocessor extends HQ {
     public MapPreProcessor map;
     public RallyPointProcessor rally;
     protected boolean finishedPostCalc = false;
-    private int donePath = 0;
     private boolean initStar = false;
-    private AStar a;
+    public AStar a = null;
 
     public HQPreprocessor(RobotController rc, RobotInformation info) {
         super(rc, info);
